00347cebb66070c406ae2d4e144814d91450d28c
Andris Stikans <andris.stikans@gmail.com>
2025-05-06 09:16:07 +0300

    Remove unused projection initialization from Application factory

--- spec/factories/applications.rb
+++ spec/factories/applications.rb
@@ -3,8 +3,5 @@ FactoryBot.define do
     association :job
     candidate_name { Faker::Name.name }
     state { [ "applied", "interview", "hired", "rejected" ].sample }
-
-    # Initialize the projection hash for store_accessor fields
-    projection { { notes_count: rand(0..10) } }
   end
 end
